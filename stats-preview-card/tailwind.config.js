fb73907d3dad00973f243cb4e2cb1e3453c8f3e3
Jeanie Chea <chea.jeanie@gmail.com>
2022-03-05 18:02:21 -0800

    Stats Preview Card - start

---
+++ stats-preview-card/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './layouts/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        
+      },
+    },
+  },
+  plugins: [],
+}
