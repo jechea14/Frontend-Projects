7431612f79d84dbb12f68568c8e8a31c2c2b75c6
Jeanie Chea <chea.jeanie@gmail.com>
2022-09-30 16:48:46 -0700

    Add pokemon search and mobile menu

--- todoCrud/index.js
+++ todoCrud/index.js
@@ -48,3 +48,53 @@ const ul = document.getElementById("productDropdown")
 const dropdown = () => {
     ul.classList.toggle("show")
 }
+
+const mobileMenu = document.getElementById('mobileMenu')
+const openMenu = () => {
+    mobileMenu.classList.toggle('show')
+}
+
+
+//--------------------------------------------
+const pokemonInput = document.getElementById("pokemonInput")
+const pokemonButton = document.getElementById("pokemonButton")
+const pokemonResult = document.getElementById("pokemonResult")
+const newP = document.createElement("p")
+const pokeImage = document.createElement("img")
+
+const getPokemon = async() => {
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput.value}`)
+        const data = await res.json()
+        console.log(data)
+        return [data.name, data.sprites.front_default, data.types]
+    } catch(e) {
+        const newP = document.createElement("p")
+        return ["pokemon not found"]
+    }
+
+}
+
+const searchPokemon = async() => {
+
+    const [pokeName, pokeSprite, pokeType] = await getPokemon()
+
+    pokemonResult.innerHTML = ''
+    pokeImage.src = pokeSprite
+    newP.innerHTML = pokeName
+    pokemonResult.append(newP)
+    pokemonResult.append(pokeImage)
+    const pokeTypeUL = document.createElement("ul")
+
+
+    for(type of pokeType) {
+        console.log(type.type.name)
+        const pokeTypeLI = document.createElement("li")
+        pokeTypeLI.innerHTML = type.type.name
+        pokeTypeUL.append(pokeTypeLI)
+    }
+
+    pokemonResult.append(pokeTypeUL)
+
+    // console.log(pokeType)
+}
