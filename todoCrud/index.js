168c363c9085142c935e8fe07c4978f8d00fcfd1
Jeanie Chea <chea.jeanie@gmail.com>
2022-10-05 21:51:14 -0700

    Update index.js

--- todoCrud/index.js
+++ todoCrud/index.js
@@ -52,6 +52,11 @@ const dropdown = () => {
 const mobileMenu = document.getElementById('mobileMenu')
 const openMenu = () => {
     mobileMenu.classList.toggle('show')
+
+}
+const contactMenu = document.getElementById("contactDropdown")
+const openContact = () => {
+    contactMenu.classList.toggle("showContact")
 }
 
 
@@ -67,7 +72,7 @@ const getPokemon = async() => {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput.value}`)
         const data = await res.json()
         console.log(data)
-        return [data.name, data.sprites.front_default, data.types]
+        return [data.name, data.sprites.front_default, data.types, data.stats]
     } catch(e) {
         const newP = document.createElement("p")
         return ["pokemon not found"]
@@ -77,24 +82,40 @@ const getPokemon = async() => {
 
 const searchPokemon = async() => {
 
-    const [pokeName, pokeSprite, pokeType] = await getPokemon()
-
+    const [pokeName, pokeSprite, pokeType, pokeStats] = await getPokemon()
+    pokemonInput.value = ''
     pokemonResult.innerHTML = ''
+
+    //pokemon name and sprite image
     pokeImage.src = pokeSprite
     newP.innerHTML = pokeName
     pokemonResult.append(newP)
     pokemonResult.append(pokeImage)
     const pokeTypeUL = document.createElement("ul")
 
-
+    // pokemon type
     for(type of pokeType) {
         console.log(type.type.name)
         const pokeTypeLI = document.createElement("li")
         pokeTypeLI.innerHTML = type.type.name
         pokeTypeUL.append(pokeTypeLI)
     }
-
     pokemonResult.append(pokeTypeUL)
 
-    // console.log(pokeType)
+    //pokemon base stats
+    for(let i = 0; i < pokeStats.length; i++) {
+        const stats = document.createElement("p")
+        const effort = document.createElement("p")
+        stats.innerText = pokeStats[i].stat.name + ": " + pokeStats[i].base_stat
+        effort.innerText = "effort: " + pokeStats[i].effort
+
+        pokemonResult.append(stats)
+        pokemonResult.append(effort)
+    }
+}
+
+//----------------------------------
+const content = document.getElementsByClassName(".content")
+const openAccordian = () => {
+    content.classList.toggle('show')
 }
