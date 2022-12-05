0a8da5721a2847e35d5b4a442786fce9d42c0ddb
Jeanie Chea <chea.jeanie@gmail.com>
2022-12-05 00:13:54 -0800

    Finish shopping cart

--- shoppingcart2/src/CartItem/CartItem.tsx
+++ shoppingcart2/src/CartItem/CartItem.tsx
@@ -11,20 +11,20 @@ type Props = {
 
 const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => {
     return (
-        <div>
-            <div>
+        <div className='flex justify-between border-b-blue-400'>
+            <div className=''>
                 <h3>{item.title}</h3>
-                <div>
+                <div className='flex justify-between'>
                     <p>Price: ${item.price}</p>
                     <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
                 </div>
-                <div>
-                    <Button size='small' disableElevation variant='contained' onClick={() => removeFromCart(item.id)}>-</Button>
+                <div className='flex justify-between'>
+                    <Button size='small' disableElevation variant='contained' onClick={() => removeFromCart(item.id)} className='flex justify-between'>-</Button>
                     <p>{item.amount}</p>
-                    <Button size='small' disableElevation variant='contained' onClick={() => addToCart(item)}>+</Button>
+                    <Button size='small' disableElevation variant='contained' onClick={() => addToCart(item)} className='flex justify-between'>+</Button>
                 </div>
             </div>
-            <img src={item.image} alt={item.title}/>
+            <img src={item.image} alt={item.title} className='max-w-xs object-cover ml-10'/>
         </div>
     )
 }
